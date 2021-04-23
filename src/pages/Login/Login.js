d6b064dd7a6666e97d222a26527a3ede1a1db74a
Fernando Cesar <cmnando.silva@gmail.com>
2021-04-22 22:44:54 -0300

    Feat: frontend da página feito

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Login.scss";
+import TextBox from "../../components/TextBox";
+import TextBoxPassword from "../../components/TextBoxPassword";
 
 function Login() {
   return (
-    <div>
-      <h1> Login </h1>
+    <div className="screen">
+      <div className="login">
+        <h1>Login</h1>
+        <div className="text1">
+          <TextBox />
+        </div>
+        <div className="text1">
+          <TextBoxPassword />
+        </div>
+        <div className="botoes">
+          <div className="b1">
+            <button type="entrar">Entrar</button>
+          </div>
+          <div className="link">
+            <Link to="login"> Esqueceu a senha? </Link>
+          </div>
+          <br />
+          <div className="b2">
+            <button type="cadastro">Cadastrar-se</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Login;
