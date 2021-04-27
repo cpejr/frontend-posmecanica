c6f77d89e4a7b4a38ed65a3668d944894b4e99be
Yasmim Carla <yasmimcarla@yahoo.com.br>
2021-04-27 20:05:38 -0300

    Feat:Pagina de esqueci a senha, header e footer

---
+++ src/components/CampoText/campotxt.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiTextField-root": {
+      margin: theme.spacing(1),
+      width: 200,
+    },
+  },
+}));
+
+export default function TextFieldSizes() {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <div>
+        <TextField
+          label="Size"
+          id="outlined-size-small"
+          defaultValue="Small"
+          variant="outlined"
+          size="small"
+        />
+      </div>
+    </form>
+  );
+}
