e5e1c4e06879bc447637701ef237dfd6b32119c4
Fernando Cesar <cmnando.silva@gmail.com>
2021-05-06 03:20:12 -0300

    Feat: Mudanças scss no dashboard adm

--- src/components/Dashboards/Administrator/BoxDashboardAdministrator.js
+++ src/components/Dashboards/Administrator/BoxDashboardAdministrator.js
@@ -9,14 +9,13 @@ function BoxDashboardAdministrator() {
     const selectiveProcess = await managerService.getByIdSelectiveProcess('ce96ed27-7d30-4157-bc63-bc9bd2a21dc6');
     const totalCandidates = await managerService.getCandidates('candidate_process_id', selectiveProcess.candidate_process_id);
     setCandidates(totalCandidates);
-    console.log(candidates);
   }, []);
   return (
-    <div className="tela">
-      <div className="tituloPrincipal"> Processo Seletivo </div>
-      <div className="tituloSecundario"> Lista de Inscritos: </div>
-      <div className="aba">
-        <div className="inscritos">
+    <div className="BDAtela">
+      <div className="BDAtituloPrincipal"> Processo Seletivo </div>
+      <div className="BDAtituloSecundario"> Lista de Inscritos: </div>
+      <div className="BDAaba">
+        <div className="BDAinscritos">
           {candidates.map((candidate, key) => (
             <InscritoPS name={candidate.candidate_name} id={candidate.candidate_id} chave={key} />
           ))}
