dba3b6e4d1de6b53d8e3d849bb821cfa3d4286a3
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-07-23 18:43:43 -0300

    Fix error value

--- src/Utils/genres.js
+++ src/Utils/genres.js
@@ -2,7 +2,7 @@ const Genres = [
   { label: 'Mulher cis', value: 'mulher cis' },
   { label: 'Mulher trans', value: 'mulher trans' },
   { label: 'Homem cis', value: 'homem cis' },
-  { label: 'Homem trans', value: 'homem cis' },
+  { label: 'Homem trans', value: 'homem trans' },
   { label: 'Não-binário', value: 'nao-binario' },
   { label: 'Agênero', value: 'agenero' },
   { label: 'Outro', value: 'outro' },
