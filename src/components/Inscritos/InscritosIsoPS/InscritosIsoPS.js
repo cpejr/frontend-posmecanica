9bd37fc5b45910ef61bfdce26cad2bd667fbf802
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-08-14 18:13:23 -0300

    Modal implementado

--- src/components/Inscritos/InscritosIsoPS/InscritosIsoPS.js
+++ src/components/Inscritos/InscritosIsoPS/InscritosIsoPS.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconContext } from 'react-icons/lib';
 import { BiUserCircle } from 'react-icons/bi';
 import Button from '@material-ui/core/Button';
+import InfoModal from '../../../pages/SentDocuments/InfoModal';
 import * as managerService from '../../../services/manager/managerService';
 import './InscritosIsoPS.scss';
 
-function InscritosIsoPS({ isoCandidates, setIsoCandidates, candidate }) {
+function InscritosIsoPS({
+  isoCandidates, setIsoCandidates, candidate,
+}) {
+// console.log('🚀 ~ file: InscritosIsoPS.js ~ line 10 ~ InscritosIsoPS ~ location', location.state);
+  const [showInfoModal, setShowInfoModal] = useState(false);
+
+  const handleClickClose = () => {
+    setShowInfoModal(false);
+  };
+
   const handleClick = async (e) => {
     const buttonName = e.currentTarget.id;
     if (buttonName === 'Deferir') {
@@ -31,14 +41,19 @@ function InscritosIsoPS({ isoCandidates, setIsoCandidates, candidate }) {
         <Button className="isoPsConfirmButton" id="Deferir" onClick={(e) => handleClick(e)} variant="contained">Deferir</Button>
         <Button className="isoPsDenyButton" id="Indeferir" onClick={(e) => handleClick(e)} variant="contained">Indeferir</Button>
       </div>
-      <a
-        href="https://www.google.com"
-        target="_blank"
-        rel="noreferrer"
+      <div className="divButtonSituationStudent">
+        <button type="button" className="buttonSituationStudent" onClick={() => setShowInfoModal(true)}>Ver situação do aluno</button>
+      </div>
+      {showInfoModal && (
+      <InfoModal
+        conteudo={candidate}
+        close={handleClickClose}
         className="isoPsLinkButton"
       >
         Ver situação do aluno
-      </a>
+      </InfoModal>
+      )}
+
     </div>
   );
 }
