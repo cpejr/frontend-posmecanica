d82fa5ae37af94fac5ea3cd928cccc37dcab8e50
Matheus <matheusmiranda98@live.com>
2021-10-20 23:59:49 -0300

    Removendo alteração semesters

--- src/utils/semesters.js
+++ src/utils/semesters.js
@@ -1,5 +1,5 @@
 const Semesters = [
-  { label: 'Todos', value: 'Todos' },
+  { label: 'Todos', value: '' },
   { label: '2020/1', value: '2020/1' },
   { label: '2020/2', value: '2020/2' },
   { label: '2021/1', value: '2021/1' },
