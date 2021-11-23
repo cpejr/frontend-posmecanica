f617a23f36d58d9ac6e0d686146975823e7adde2
Gustavo Santos <gustavosantos@cpejr.com.br>
2021-11-23 20:16:26 -0300

    Mudando de ativa para ofertada

--- src/utils/semester.js
+++ src/utils/semester.js
@@ -1,5 +1,5 @@
 const semester = [
-  { label: 'Ativa', value: 'ATIVO' },
+  { label: 'Ofertada', value: 'OFERTADA' },
   { label: 'Não Ofertada', value: 'NAO_OFERTADO' },
 ];
 export default semester;
