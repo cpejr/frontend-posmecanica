60da8108c9be4ac380ddfb8998e1979fa24ce1b2
Giovanni Pantuzzo <giovannipantuzzo@cpejr.com.br>
2021-11-23 23:54:43 -0300

    fix

--- src/components/FormsDI/FormsDI.js
+++ src/components/FormsDI/FormsDI.js
@@ -41,7 +41,7 @@ function Forms({
 
   useEffect(async () => {
     managerService.getDisciplines('discipline_is_isolated', true).then((resp) => {
-      resp = resp.filter((item) => item.discipline_semester === 'ATIVO');
+      resp = resp.filter((item) => item.discipline_semester === 'OFERTADO');
       const disciplinas = [];
       disciplinas.push({ label: 'Nenhuma', value: '' });
       resp.forEach((object) => {
